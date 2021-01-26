677c371a7cbc0f9421d417b03ea7dfdad187221c
shihyi <brvast@gmail.com>
2021-01-26 19:39:26 +1300

    add service

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoListService } from './todo-list.service';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private todoListService: TodoListService) { }
 
   ngOnInit(): void {
   }
 
+  addTodo(inputRef: any): void {
+
+    console.log(inputRef.value)
+    const todo = inputRef.value.trim();
+
+    if (todo) {
+      this.todoListService.add(todo);
+      inputRef.value = '';
+    }
+
+  }
+
+  getList(): string[] {
+    return this.todoListService.getList();
+  }
 }
